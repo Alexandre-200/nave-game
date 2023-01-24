94b7ac9f8cfc666794901c1fd7f1cc987bcc6e2f
Alexandre-200 <alexandre_200@outlook.com.br>
2023-01-24 02:49:00 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 var diryJ, dirxJ, jog, velJ, pjx, pjy;
+var velT;
 var tamTelaW, tamTelaH;
 var jogo;
 var frames;
+var contBombas, painelContBombas, velB, tmpCriaBomba;
+var bombasTotal;
+var vidaPlaneta, barraPlaneta;
+var ie, isom;
+var telaMsg;
 
 function teclaDw() {
   var tecla = event.keyCode;
@@ -21,7 +27,7 @@ function teclaDw() {
   if (tecla == 32) {
     //ESPAÇO
     //TIRO
-    atira(pjx+17, pjy);
+    atira(pjx + 17, pjy);
   }
 }
 
@@ -31,6 +37,40 @@ function teclaUp() {
     diryJ = 0;
   } else if (tecla == 39 || tecla == 37) {
     dirxJ = 0;
+  }
+}
+
+function criaBomba() {
+  if (jogo) {
+    var y = 0;
+    var x = Math.random() * tamTelaW;
+    var bomba = document.createElement("div");
+    var att1 = document.createAttribute("class");
+    var att2 = document.createAttribute("style");
+    att1.value = "bomba";
+    att2.value = "top:" + y + "px; left:" + x + "px;";
+    bomba.setAttributeNode(att1);
+    bomba.setAttributeNode(att2);
+    document.body.appendChild(bomba);
+    contBombas--;
+  }
+}
+
+function controlaBomba() {
+  bombasTotal = document.getElementsByClassName("bomba");
+  var tam = bombasTotal.length;
+  for (var i = 0; i < tam; i++) {
+    if (bombasTotal[i]) {
+      var pi = bombasTotal[i].offsetTop;
+      pi += velB;
+      bombasTotal[i].style.top = pi + "px";
+      if (pi > tamTelaH) {
+        vidaPlaneta -= 10;
+        criaExplossao(2, bombasTotal[i].offsetLeft, null);
+
+        bombasTotal[i].remove();
+      }
+    }
   }
 }
 
@@ -45,6 +85,92 @@ function atira(x, y) {
   document.body.appendChild(t);
 }
 
+function controleTiros() {
+  var tiros = document.getElementsByClassName("tiroJog");
+  var tam = tiros.length;
+  for (var i = 0; i < tam; i++) {
+    if (tiros[i]) {
+      var pt = tiros[i].offsetTop;
+      pt -= velT;
+      tiros[i].style.top = pt + "px";
+      colisaoTiroBomba(tiros[i]);
+      if (pt < 0) {
+        //document.body.removeChild(tiros[i]);
+        tiros[i].remove();
+      }
+    }
+  }
+}
+
+function colisaoTiroBomba(tiro) {
+  var tam = bombasTotal.length;
+  for (var i = 0; i < tam; i++) {
+    if (bombasTotal[i]) {
+      if (
+        tiro.offsetTop <= bombasTotal[i].offsetTop + 40 && //cima tiro com baixo bomba
+        tiro.offsetTop + 6 >= bombasTotal[i].offsetTop && //baixo tiro com cima bomba
+        tiro.offsetLeft <= bombasTotal[i].offsetLeft + 24 && //esquerda tiro com direita bomba
+        tiro.offsetLeft + 6 >= bombasTotal[i].offsetLeft //direita tiro com esquerda bomba
+      ) {
+        criaExplossao(
+          1,
+          bombasTotal[i].offsetLeft - 25,
+          bombasTotal[i].offsetTop
+        );
+        bombasTotal[i].remove();
+        tiro.remove();
+      }
+    }
+  }
+}
+
+function criaExplossao(tipo, x, y) {
+  //tipo 1=AR, 2=TERRA
+  if (document.getElementById("explosao" + ie - 5)) {
+    document.getElementById("explosao" + ie - 5).removeAttributeNode();
+  }
+  var explosao = document.createElement("div");
+  var img = document.createElement("img");
+  var som = document.createElement("audio");
+
+  var att1 = document.createAttribute("class");
+  var att2 = document.createAttribute("style");
+  var att3 = document.createAttribute("id");
+
+  var att4 = document.createAttribute("src");
+
+  var att5 = document.createAttribute("src");
+  var att6 = document.createAttribute("id");
+
+  att3.value = "explosao" + ie;
+  if (tipo == 1) {
+    att1.value = "explosaoAr";
+    att2.value = "top:" + y + "px;left:" + x + "px";
+    att4.value = "../img/explosao_ar.gif?" + new Date();
+  } else {
+    att1.value = "explosaoChao";
+    att2.value = "top:" + (tamTelaH - 57) + "px;left:" + (x - 17) + "px";
+    att4.value = "../img/explosao_chao.gif?" + new Date();
+  }
+  att5.value = "../img/exp1.mp3?" + new Date();
+  att6.value = "som" + isom;
+
+  explosao.setAttributeNode(att1);
+  explosao.setAttributeNode(att2);
+  explosao.setAttributeNode(att3);
+
+  img.setAttributeNode(att4);
+  som.setAttributeNode(att5);
+  som.setAttributeNode(att6);
+  explosao.appendChild(img);
+  explosao.appendChild(som);
+
+  document.body.appendChild(explosao);
+  document.getElementById("som" + isom).play();
+  isom++;
+  ie++;
+}
+
 function controlaJogador() {
   pjy += diryJ * velJ;
   pjx += dirxJ * velJ;
@@ -52,12 +178,54 @@ function controlaJogador() {
   jog.style.left = pjx + "px";
 }
 
+function gerenciaGame() {
+  barraPlaneta.style.width = vidaPlaneta + "px";
+  if (contBombas <= 0) {
+    jogo = false;
+    clearInterval(tmpCriaBomba);
+    telaMsg.style.backgroundImage = "url('../img/vitoria.jpg')";
+    telaMsg.style.display = "block";
+  }
+  if (vidaPlaneta <= 0) {
+    jogo = false;
+    clearInterval(tmpCriaBomba);
+
+    telaMsg.style.display = "block";
+    telaMsg.style.backgroundImage = "url('./img/derrota.jpg')";
+  }
+}
+
 function gameLoop() {
   if (jogo) {
     //funcoes de controle
     controlaJogador();
+    controleTiros();
+    controlaBomba();
   }
+  gerenciaGame();
   frames = requestAnimationFrame(gameLoop);
+}
+
+function reinicia() {
+  bombasTotal = document.getElementsByClassName("bomba");
+  var tam = bombasTotal.length;
+  for (var i = 0; i < tam; i++) {
+    if (bombasTotal[i]) {
+      bombasTotal[i].remove();
+    }
+  }
+  telaMsg.style.display = "none";
+  clearInterval(tmpCriaBomba);
+  cancelAnimationFrame(frames);
+  vidaPlaneta = 300;
+  pjx = tamTelaW / 2;
+  pjy = tamTelaH / 2;
+  jog.style.top = pjy + "px";
+  jog.style.lft = pjx + "px";
+  contBombas = 150;
+  jogo = true;
+  tmpCriaBomba = setInterval(criaBomba, 1700);
+  gameLoop();
 }
 
 function inicia() {
@@ -70,15 +238,33 @@ function inicia() {
   dirxJ = diryJ = 0;
   pjx = tamTelaW / 2;
   pjy = tamTelaH / 2;
-  velJ = 5;
+  velJ = velT = 5;
   jog = document.getElementById("navJog");
 
-  console.log("jog exist? " + jog);
-  jog.style.color = "red";
   jog.style.top = pjy + "px";
   jog.style.left = pjx + "px";
 
-  gameLoop();
+  //controle de bombas
+ 
+
+  contBombas = 150;
+  velB = 3;
+  
+
+  //controles do planeta
+  vidaPlaneta = 300;
+  barraPlaneta = document.getElementById("barraPlaneta");
+  barraPlaneta.style.width = vidaPlaneta + "px";
+  //controle explsao
+  ie = isom = 0;
+
+  //telas
+  telaMsg = document.getElementById("telaMsg");
+  telaMsg.style.backgroundImage = "url('../img/intro.jpg')";
+  telaMsg.style.display = "block";
+  document.getElementById("btnJogar").addEventListener("click", reinicia);
+
+  
 }
 
 window.addEventListener("load", inicia);
